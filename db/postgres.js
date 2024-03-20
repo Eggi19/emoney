5922252863005126ee2f5467eb0d5e255ff8e101
Eggi Eka Yapari <eggiyapari19@gmail.com>
2024-03-20 23:49:46 +0700

    update

--- db/postgres.js
+++ db/postgres.js
@@ -1,16 +1,32 @@
-const { Pool } = require('pg')
+const { Pool, Client } = require('pg')
 require('dotenv').config();
 
-let client
 
+//local db
+// const pgClient = () => {
+//   let client
+//   if (!client) {
+//     client = new Pool({
+//       host: process.env.ENV_HOST,
+//       port: process.env.ENV_PORT,
+//       user: process.env.ENV_USER_DB,
+//       password: process.env.ENV_PASSWORD_DB,
+//       database: process.env.ENV_NAME_DB,
+//     });
+//   }
+//   return client;
+// };
+
+//remote db
 const pgClient = () => {
+  let client
   if (!client) {
     client = new Pool({
-      host: process.env.ENV_HOST,
-      port: process.env.ENV_PORT,
-      user: process.env.ENV_USER_DB,
-      password: process.env.ENV_PASSWORD_DB,
-      database: process.env.ENV_NAME_DB,
+      host: process.env.ENV_HOST_PRODUCTION,
+      port: process.env.ENV_PORT_PRODUCTION,
+      user: process.env.ENV_USER_PRODUCTION,
+      password: process.env.ENV_PASSWORD_PRODUCTION,
+      database: process.env.ENV_NAME_PRODUCTION,
     });
   }
   return client;
